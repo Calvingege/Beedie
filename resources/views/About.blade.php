3572ae9fd285e3cc478012a96268b03686481faf
Spicy <bryan008@binus.ac.id>
2022-06-14 13:36:14 +0700

    ganti tampilan dan fix bug
    tampilan sesuai figma dan gambar udahnya bisa

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -9,67 +9,98 @@
 </head>
 <body>
 
-<style>
+    <style>
+        div {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      margin-right: 20px;
+      margin-left: 20px;
+        }
+
         #navbar{
-    background-color: yellowgreen;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
+        background-color: #222831;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    
+    .logo img{
+        height: 50px;
+    }
 
-ul{
-    color: white;
-    display: flex;
-    height: 10vh;
-    /* align-items: center; */
-}
+    .profile img{
+        height: 50px;
+    }
 
-li{
-    list-style: none;
-    margin: 0 30px;
-    font-size: 25px;
-}
+    ul{
+        color: #B67300;
+        display: flex;
+        height: 10vh;
+        /* align-items: center; */
+    }
+    
+    li{
+        list-style: none;
+        margin: 0 30px;
+        font-size: 25px;
+    }
+    
+    a{
+        font-weight: bolder; 
+        color: #B67300;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+    }
 
-a{
-    font-weight: bold;
-    color: green;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-}
+    a:hover{
+        color: #7e5102
+    }
+    
+    #hero-section{
+        height: 70vh;
+        background-color: white;
+        background-size: cover;
+        /* background-repeat: ; */
+        display: flex;  
+        justify-content: center;
+        flex-direction: column;
+        padding-left: 200px;
+    }
+    
+    #picture-selection{
+        float: right;
+        padding: 10px;
+        padding-top: 120px;
+        padding-right: 250px;
+    }
+    
+    #pre-selection{
+        padding-left: 88px;
+    }
+        </style>
+    
+    </head>
+    <body>
+    <div id="navbar">
+        <div class="logo">
+            <a href="/home"><img src="/assets/biddie.png" alt=""></a>
+        </div>
 
-#hero-section{
-    height: 70vh;
-    background-color: white;
-    background-size: cover;
-    /* background-repeat: ; */
-    display: flex;  
-    justify-content: center;
-    flex-direction: column;
-    padding-left: 200px;
-}
-
-#picture-selection{
-    float: right;
-    padding: 10px;
-    padding-top: 120px;
-    padding-right: 250px;
-}
-
-#pre-selection{
-    padding-left: 88px;
-}
-</style>
-<div id="navbar">
-        <ul>
-        <a href="/home"><li>Home</li></a>
-        <a href="/login"><li>login</li></a>
-        <a href="/create/bid"><li>Buy Item</li></a>
-        <a href="/create/adds"><li>Sel Item</li></a>
-        <a href=""><li>about us</li></a>
-        </ul>
-      </div>
-
+          <div>
+            <ul>
+            <a href="/home"><li>Home</li></a>
+            <a href="/create/bid"><li>Buy Item</li></a>
+            <a href="/create/adds"><li>Sell Item</li></a>
+            <a href="/about"><li>about us</li></a>
+            </ul>
+          </div>
+            
+          <div class="profile">
+            <a href="/dashboard"><img src="/assets/profile.svg" alt=""></a>
+          </div>
+          
+    </div>
     <!-- <div id="picture-selection">
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<img src="../assets/online-auction-4.23.jpg" alt="ini gambar" style="width: 300px; height: 300px" class="floatRight">
--- resources/views/CreateBid.blade.php
+++ resources/views/CreateBid.blade.php
@@ -9,76 +9,97 @@
 
     <!-- Bikin merjinnya -->
     <style>
-    div {
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-right: 20px;
-  margin-left: 20px;
+        div {
+      margin-top: 10px;
+      margin-bottom: 10px;
+      margin-right: 20px;
+      margin-left: 20px;
+        }
+
+        #navbar{
+        background-color: #222831;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
-    </style>
+    
+    .logo img{
+        height: 50px;
+    }
 
-</head>
-<body>
-<style>
-  #navbar{
-    background-color: greenyellow;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
+    .profile img{
+        height: 50px;
+    }
 
-ul{
-    color: white;
-    display: flex;
-    height: 10vh;
-    /* align-items: center; */
-}
+    ul{
+        color: #B67300;
+        display: flex;
+        height: 10vh;
+        /* align-items: center; */
+    }
+    
+    li{
+        list-style: none;
+        margin: 0 30px;
+        font-size: 25px;
+    }
+    
+    a{
+        font-weight: bolder; 
+        color: #B67300;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+    }
 
-li{
-    list-style: none;
-    margin: 0 30px;
-    font-size: 25px;
-}
+    a:hover{
+        color: #7e5102
+    }
+    
+    #hero-section{
+        height: 70vh;
+        background-color: white;
+        background-size: cover;
+        /* background-repeat: ; */
+        display: flex;  
+        justify-content: center;
+        flex-direction: column;
+        padding-left: 200px;
+    }
+    
+    #picture-selection{
+        float: right;
+        padding: 10px;
+        padding-top: 120px;
+        padding-right: 250px;
+    }
+    
+    #pre-selection{
+        padding-left: 88px;
+    }
+        </style>
+    
+    </head>
+    <body>
+    <div id="navbar">
+        <div class="logo">
+            <a href="/home"><img src="/assets/biddie.png" alt=""></a>
+        </div>
 
-a{
-    font-weight: bold;
-    color: green;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-}
-
-#hero-section{
-    height: 70vh;
-    background-color: white;
-    background-size: cover;
-    /* background-repeat: ; */
-    display: flex;  
-    justify-content: center;
-    flex-direction: column;
-    padding-left: 200px;
-}
-
-#picture-selection{
-    float: right;
-    padding: 10px;
-    padding-top: 120px;
-    padding-right: 250px;
-}
-
-#pre-selection{
-    padding-left: 88px;
-}
-</style>
-<div id="navbar">
-        <ul>
-        <a href="/home"><li>Home</li></a>
-        <a href=""><li>login</li></a>
-        <a href="/create/bid"><li>Buy Item</li></a>
-        <a href="/create/adds"><li>Sel Item</li></a>
-        <a href="/about"><li>about us</li></a>
-        </ul>
-      </div>
+          <div>
+            <ul>
+            <a href="/home"><li>Home</li></a>
+            <a href="/create/bid"><li>Buy Item</li></a>
+            <a href="/create/adds"><li>Sell Item</li></a>
+            <a href="/about"><li>about us</li></a>
+            </ul>
+          </div>
+            
+          <div class="profile">
+            <a href="/dashboard"><img src="/assets/profile.svg" alt=""></a>
+          </div>
+          
+    </div>
 
 <form action="{{route('StoreBid')}}" method="POST" enctype="multipart/form-data">
 @csrf
