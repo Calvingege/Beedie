1ea2e6f4d94767f4a0d08404f05174b40266be12
Spicy <bryan008@binus.ac.id>
2022-06-17 15:24:19 +0700

    'history'
    bug fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
-                    <a href="/home"><p style="color: blue">Continue</p></a>
                 </div>
             </div>
         </div>
